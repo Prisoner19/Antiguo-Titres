f278d2524ca953f55486299ae3ed2c51fa5a0f05
Cesar Zegarra <czegarram@gmail.com>
2014-01-09 02:21:29 -0500

    Fixed Acoplamiento de Figuras

--- Assets/Scripts/Mecánicas/sPrueba.cs
+++ Assets/Scripts/Mecánicas/sPrueba.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class sPrueba : MonoBehaviour {
 
-	private bool colision=false;
+	public static bool colision=false;
+	private static bool trigger=false;
 	// Use this for initialization
 	void Start () {
 	
@@ -17,41 +18,5 @@ public class sPrueba : MonoBehaviour {
 	void OnMouseDown(){
 		Debug.Log("En clon");
 	}
-	/*
-	void OnTriggerEnter2D(Collider2D other) {
-		if (!other.name.StartsWith ("B")) {
-			if(!colision){
-				Debug.Log (other.name);
-			}
-			colision=true;
-		}
-	}
 
-	void OnTriggerExit2D(Collider2D other) {
-		if (!other.name.StartsWith ("B")) {
-			if(colision){
-				Debug.Log (other.name);
-			}
-			colision=false;
-		}
-	}*/
-
-	void OnCollisionEnter2D(Collision2D coll) {
-		if (!colision) {
-			if(coll.collider.name == "Bloque"){
-				this.transform.parent.GetComponent<sClon>().figura.GetComponent<sFigura>().chocaPared=true;
-				Debug.Log("bloque");
-			}
-			colision=true;
-		}
-	}
-
-	void OnCollisionExit2D(Collision2D coll) {
-		if (colision) {
-			if(coll.collider.name == "Bloque"){
-				this.transform.parent.GetComponent<sClon>().figura.GetComponent<sFigura>().chocaPared=false;
-			}
-			colision=false;
-		}
-	}
 }
