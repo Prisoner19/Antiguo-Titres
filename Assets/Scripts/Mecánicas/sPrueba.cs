f4bbf3024fbdea49e7bb66cfe9cae25dbce7f1e8
Cesar Zegarra <czegarram@gmail.com>
2014-01-09 00:31:17 -0500

    sPrueba changed
    Just a test for fix some bugs

--- Assets/Scripts/Mecánicas/sPrueba.cs
+++ Assets/Scripts/Mecánicas/sPrueba.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class sPrueba : MonoBehaviour {
 
+	private bool colision=false;
 	// Use this for initialization
 	void Start () {
 	
@@ -15,5 +16,24 @@ public class sPrueba : MonoBehaviour {
 
 	void OnMouseDown(){
 		Debug.Log("En clon");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name.StartsWith ("B")) {
+			if(!colision){
+				Debug.Log (other.gameObject.transform.parent);
+				Debug.Log ("unico-entro");
+			}
+			colision=true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.name.StartsWith ("B")) {
+			if(colision){
+				Debug.Log ("unico-sale");
+			}
+			colision=false;
+		}
 	}
 }
