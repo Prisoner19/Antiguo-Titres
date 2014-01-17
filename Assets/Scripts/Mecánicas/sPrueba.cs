e2d300d2a16fbf5e28d777e4e604277b3c4ac02d
Prisoner19 <clz.german@gmail.com>
2014-01-16 22:04:49 -0500

    Rollin' back 3: Te odio Chato

--- Assets/Scripts/Mecánicas/sPrueba.cs
+++ Assets/Scripts/Mecánicas/sPrueba.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class sPrueba : MonoBehaviour {
 
+<<<<<<< HEAD
 	public static bool colision=false;
 	private static bool trigger=false;
+=======
+>>>>>>> parent of f4bbf30... sPrueba changed
 	// Use this for initialization
 	void Start () {
 	
@@ -18,5 +21,8 @@ public class sPrueba : MonoBehaviour {
 	void OnMouseDown(){
 		Debug.Log("En clon");
 	}
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of f4bbf30... sPrueba changed
 }
